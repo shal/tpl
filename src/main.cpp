d84b6bf2766ba7fa48579780b3c5b2d8ad662fdc
Ali Shanaakh <github@shal.dev>
2020-06-19 00:11:18 +0300

    Update with working solution

--- src/main.cpp
+++ src/main.cpp
@@ -5,97 +5,29 @@
 
 using namespace std;
 
-// This is polymorphic function #1.
-template <typename T>
-bool is_integer(T arg)
-{
-    return false;
-}
-
-template <>
-bool is_integer<int>(int arg)
-{
-    return true;
-}
-
-// This is polymorphic function #2.
-template <typename T>
-bool is_string(T arg)
-{
-    return false;
-}
-
-template <>
-bool is_string<string>(string arg)
-{
-    return true;
-}
-
-// Implement in C++ function that takes polymorphic function as an argument.
-template <typename T>
-vector<bool> check_each(vector<T> arr, bool (*check)(T))
-{
-    vector<bool> res;
-    for (auto x : arr) {
-        res.push_back(check(x));
+// Polymorphic class.
+template<typename T>
+class Functor {
+public:
+    T operator () (T x) {
+        return x;
     }
+};
 
-    return res;
-}
-
-void example_works()
+// Function that takes polymorphic functor as an argument.
+template<template<typename T> class G>
+pair<int,double> some_func()
 {
-    vector<int> input1({ 1, 10, 20 });
-
-    cout << "is_integer<int>: ";
-    for (auto x : check_each(input1, is_integer)) {
-        cout << (x ? "yes" : "no") << " ";
-    }
-    cout << endl;
-
-    cout << "is_string<int>: ";
-    for (auto x : check_each(input1, is_string)) {
-        cout << (x ? "yes" : "no") << " ";
-    }
-    cout << endl;
-
-    cout << "is_integer<string>: ";
-    vector<string> input2({ "hello", "this", "is", "strings" });
-
-    for (auto x : check_each(input2, is_integer)) {
-        cout << (x ? "yes" : "no") << " ";
-    }
-    cout << endl;
-
-    cout << "is_string<string>: ";
-    for (auto x : check_each(input2, is_string)) {
-        cout << (x ? "yes" : "no") << " ";
-    }
-    cout << endl;
+    return make_pair(G<int>()(1), G<double>()(1.1));
 }
 
-// 1. Is it possible to pass function template into such construction?
-// Yes. Use examples_works().
-
-// 2. What is the main difference with Haskell?
-// Haskell is functional language, it has different type system.
-
-// 3. Why std::function objects cannot be used?
-// Directly no, because can't get a function pointer out of an std::function.
-//
-// void example_does_not_work() {
-//     vector<string> input({ "1", "2", "3" });
-
-//     function<bool(string)> is_str = [](string) { return true; };
-//     for (auto x : check_each<string>(input, is_str)) {
-//         cout << (x ? "yes" : "no") << endl;
-// 	}
-// }
+// Is it possible to pass function template into such construction?
+// No, only functors can be used.
 
 int main(int argc, char* argv[])
 {
-    example_works();
-    // example_does_not_work();
+    pair<int, double> res = some_func<Functor>();
+    cout << res.first << " " << res.second << endl; // => 1 1.1
 
     return 0;
 }
